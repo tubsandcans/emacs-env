836e0e8a7bf86d462251d5a5170630ee9f0a881c
Joel Cornelius <joel@matasano.com>
2017-03-30 09:37:13 -0500

    preserve case when using auto-complete

--- user_config/company_config.el
+++ user_config/company_config.el
@@ -7,5 +7,5 @@
 (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
 (setq company-echo-delay 0)                          ; remove annoying blinking
 (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-
+(setq company-dabbrev-downcase nil)
 ;;; company_config.el ends here
